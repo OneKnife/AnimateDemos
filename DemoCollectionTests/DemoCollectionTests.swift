19fa22324ce990513d802a1ef876c9c8c902b9a0
melody <melody@hitour.cc>
2018-12-03 16:00:55 +0800

    Initial Commit

---
+++ DemoCollectionTests/DemoCollectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  DemoCollectionTests.swift
+//  DemoCollectionTests
+//
+//  Created by 赵一超 on 2018/12/3.
+//  Copyright © 2018年 melody. All rights reserved.
+//
+
+import XCTest
+@testable import DemoCollection
+
+class DemoCollectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
